58d57f238e17caa671d36c7ac6443dc667826ea6
keyboarddabbler <keyboarddabbler@gmail.com>
2024-01-08 14:29:27 +1300

    feat: :heavy_plus_sign: Init nuxtUI

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="text-gray-400"
+    >
+        <NuxtLoadingIndicator :height="5" :duration="3000" :throttle="400" />
+        <NuxtLayout>
+          <NuxtPage />
+        </NuxtLayout>
   </div>
 </template>
+
+<style>
+.layout-enter-active,
+.layout-leave-active {
+  transition: all 0.4s;
+}
+.layout-enter-from,
+.layout-leave-to {
+  filter: grayscale(1);
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  extends: [
+    '@nuxt/ui-pro'
+  ],
+  modules: [
+    '@nuxt/ui'
+  ],
+  app: {
+    layoutTransition: { name: 'layout', mode: 'out-in' }
+  },
+  ui: {
+    icons: ['heroicons'],
+  },
+  colorMode: {
+    preference: 'dark'
+  },
+  googleFonts: {
+    display: 'swap',
+    download: true,
+    families: {
+      'DM+Sans': [400, 500, 600, 700]
+    }
+  },
+  fontMetrics: {
+    fonts: ['DM Sans']
+  },
 })
